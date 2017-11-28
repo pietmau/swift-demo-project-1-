87c1338562d26f7df8e311a4ddae15900cb02e14
Maurizio Pietrantuono <maurizio.pietrantuon@Mac2210.local>
2017-11-28 12:16:10 +0000

    WIP

---
+++ demo1/grid/GridViewController.swift
@@ -0,0 +1,51 @@
+//
+//  GridViewController.swift
+//  demo1
+//
+//  Created by Maurizio Pietrantuono on 28/11/2017.
+//  Copyright © 2017 Maurizio Pietrantuono. All rights reserved.
+//
+
+import UIKit
+
+class GridViewController: UIViewController, MainView {
+    let SPACING = CGFloat(3)
+    private var presenter: MainPresenter?
+    @IBOutlet weak var grid: UICollectionView!
+    private var delegate: GridDelegate? = nil
+    private var layout: UICollectionViewDelegateFlowLayout? = nil
+    
+    func setResults(_ results: [Results]) {
+        (grid?.dataSource as? GridDelegate)?.results = results
+        grid.reloadData()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        print("viewWillLayoutSubviews")
+        grid.collectionViewLayout.invalidateLayout()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter = MainPresenter()
+        presenter!.view = self
+        delegate = GridDelegate()
+        grid?.dataSource = delegate
+        layout = Layout()
+        grid?.delegate = layout
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+    }
+    
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        presenter?.searchBarSearchButtonClicked(searchText)
+    }
+}
--- demo1/main/MainController.swift
+++ demo1/main/MainController.swift
@@ -1,47 +1,10 @@
 import UIKit
 
-class MainController: UIViewController, UISearchBarDelegate, MainView {
-    let SPACING = CGFloat(3)
-    private var presenter: MainPresenter?
-    @IBOutlet weak var searchBar: UISearchBar!
-    @IBOutlet weak var grid: UICollectionView!
-    private var delegate: GridDelegate? = nil
-    private var layout: UICollectionViewDelegateFlowLayout? = nil
-
-    func setResults(_ results: [Results]) {
-        (grid?.dataSource as? GridDelegate)?.results = results
-        grid.reloadData()
-    }
-
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        print("viewWillLayoutSubviews")
-        grid.collectionViewLayout.invalidateLayout()
-    }
-
+class MainController: UIViewController{
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        presenter = MainPresenter()
-        presenter!.view = self
-        searchBar?.delegate = self as? UISearchBarDelegate
-        delegate = GridDelegate()
-        grid?.dataSource = delegate
-        layout = Layout()
-        grid?.delegate = layout
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-
-    }
-
-
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        presenter?.searchBarSearchButtonClicked(searchText)
-    }
-
 }
+
 
